0a0c141abd42b11a9dd9eb1bee426a65a330c122
Jahdonah <jahdonah@Jahdonah-Pro.local>
2024-08-20 10:24:04 -0400

    feat:Create database and install mongoose dependencies

---
+++ config/connection.js
@@ -0,0 +1,9 @@
+const { connect, connection }=require('mongoose');
+
+//create database if not exist or select database to be used
+
+const connectionString= process.env.MONGODB-URI || "mongodb://127.0.0.1:27017/socialNetworkDB";
+//connect mongoose and MongoDB
+connect(connectionString);
+//EXPORTS
+module.exports=connection;
