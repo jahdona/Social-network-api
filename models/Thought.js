e7121793185ecc6e0b3a892113db0b746054a4bc
Jahdonah <jahdonah@Jahdonah-Pro.local>
2024-08-20 11:15:07 -0400

    feat:Creating Thought model

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('momngoose');
+const reactionSchema = require('./Reactions');
+
+//Thought Schema
+
+const thoughtSchema = new Schema({
+    thoughtText:{
+        type:String,
+        required:true,
+        maxlength:280,
+        minlength:1,
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now,
+    },
+    username:{
+        type:String,
+        required:true,
+    },
+    reactions:[reactionSchema],
+
+},
+{
+    toJSON:{
+        virtuals:true,
+    },
+    id:false,
+}
+);
+//increases reaction count in tghought model object when reaction are added to a thought
+thoughtSchema.virtual('reactionCount').get(function(){
+    return this.reactions.length;
+});
+//create thought model with thoughtschema
+const Thought = model('thought',thoughtSchema);
+
+module.exports= Thought;
