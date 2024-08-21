66895e992f6bd20387caf3b31b0857fedda72450
Jahdonah <jahdonah@Jahdonah-Pro.local>
2024-08-20 22:24:13 -0400

    feat:add and delete reactions

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -86,6 +86,46 @@ const thoughtController = {
       return res.status(500).json(err);
     }
   },
+  // Add reaction
+  async addReaction(req, res) {
+    try {
+      const reaction = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $addToSet: { reactions: req.body } },
+        { runValidators: true }
+      );
 
+      if (!reaction) {
+        return res.status(404).json({ message: "No thought with that ID" });
+      }
+
+      return res.status(200).json(reaction);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+
+  // Delete reaction
+  async deleteReaction(req, res) {
+    try {
+      const reaction = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $pull: { reactions: { _id: req.params.reactionId } } },
+        { runValidators: true, new: true }
+      );
+
+      if (!reaction) {
+        return res
+          .status(404)
+          .json({ message: "Check thought and reaction ID" });
+      }
+
+      return res.status(200).json(reaction);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
 };
 module.exports = thoughtController;
