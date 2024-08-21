fa4206e2e9c537fce844ae918a4fc5211cb5342a
Jahdonah <jahdonah@Jahdonah-Pro.local>
2024-08-20 22:10:19 -0400

    feat:get single thoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -13,6 +13,23 @@ const thoughtController = {
             return res.status(500).json(error);
         }
     },
+    // Get single thought
+    async getThoughts(req,res){
+        try {
+            const thought = await Thought.findOne({ _id:req.params.thoughtId});
+            if(!thought){
+                return res.status(400).json({message:"No thought with that Id"});
+            }
+            
+            return res.status(200).json(thought);
+            
+        } catch (error) {
+
+            console.log(error)
+            return res.status(500).json(error);
+        }
+    },
+   
 
 };
 module.exports = thoughtController;
