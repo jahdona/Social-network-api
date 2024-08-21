b8d835756dc3ca4fbb6e3d07654b8749bced5575
Jahdonah <jahdonah@Jahdonah-Pro.attlocal.net>
2024-08-21 07:29:18 -0400

    feat:Create  user

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,4 +33,14 @@ const userController = {
             return res.status(500).json(error);
         }
     },
+    //create user
+    async creatUser( req,res ){
+        try {
+            const user = await User.create(req.body);
+            return res.status(200).json(user);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json(error);
+        }
+    },
 }
