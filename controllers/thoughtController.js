134992f8b95685ef02008d54e73d1f25e21e8bb5
Jahdonah <jahdonah@Jahdonah-Pro.local>
2024-08-20 22:14:28 -0400

    feat:create and update thoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -29,7 +29,46 @@ const thoughtController = {
             return res.status(500).json(error);
         }
     },
-   
+    //Create thought
+    async createThought(req,res){
+        try {
+            const thought = await Thought.create(req.body);
+
+            const user = await User.findByIdAndUpdate(
+                req.body.userId,
+                { $addToSet:{thoughts:thought._id}},
+                { runValidators:true, new:true}
+
+            );
+            return res.status(200).json( thought, user );
+        } catch (error) {
+            console.log(error)
+            return res.status(500).json(error);
+            
+        }
+    },
+    //Update thought
+    async updateThought(req,res){
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id:req.params.thoughtId },
+                
+                 {$set:req.body},
+                 {runValidators:true, new:true}
+            );
+            if(!thought){
+                return res.status(400).json({message:"No thought with this Id"});
+            }
+            
+            return res.status(200).json(thought);
+
+            
+        } catch (error) {
+            console.log(error)
+            return res.status(500).json(error);
+            
+        }
+    }
 
 };
 module.exports = thoughtController;
